641ed0679295822ba24c2394df47b07a94831de0
Dakota <dakota.paige@hotmail.com>
2018-09-08 17:14:30 -0700

    Camera feature is working

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -3,9 +3,11 @@ import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 import SignInPage from '../screens/Login';
 import HomePage from '../screens/Home';
 import AuthLoadingContainer from '../screens/AuthLoading';
+import CameraScreen from '../screens/Camera';
 
 const appStack = createStackNavigator({
-  Home: HomePage
+  Home: HomePage,
+  Camera: CameraScreen
 });
 
 const authSwitch = createStackNavigator({
--- js/screens/Home/Home.js
+++ js/screens/Home/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 class Home extends Component {
   render() {
     return (
       <View>
         <Text>Home</Text>
-        <Button title="Open Camera" />
+        <Button
+          title="Open Camera"
+          onPress={() => this.props.navigation.navigate('Camera')}
+        />
       </View>
     );
   }
