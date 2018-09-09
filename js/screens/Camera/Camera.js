625e4a33717a6c0e3bb8efe85675880d9b610434
Dakota <dakota.paige@hotmail.com>
2018-09-08 18:56:16 -0700

    Continuing on camera functionality

--- js/screens/Camera/Camera.js
+++ js/screens/Camera/Camera.js
@@ -16,19 +16,44 @@ const PendingView = () => (
 );
 
 class Camera extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recording: false,
+      processing: false
+    };
+    this.stopRecording = this.stopRecording.bind(this);
+  }
+
+  takeVideo = async function(camera) {
+    const options = { quality: 0.5, base64: true };
+    this.setState({ recording: true });
+    const data = await camera.recordAsync(options);
+    //  eslint-disable-next-line
+    CameraRoll.saveToCameraRoll(video, data.uri);
+    console.log(data.uri);
+  };
+
+  stopRecording(camera) {
+    camera.stopRecording();
+    this.setState({ recording: false });
+  }
+
   render() {
+    const { recording, processing } = this.state;
     return (
       <View style={styles.container}>
         <RNCamera
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
           permissionDialogTitle={'Permission to use camera'}
           permissionDialogMessage={
             'We need your permission to use your camera phone'
           }
         >
           {({ camera, status }) => {
+            console.log('CAMERA:', status);
             if (status !== 'READY') return <PendingView />;
             return (
               <View
@@ -38,12 +63,21 @@ class Camera extends Component {
                   justifyContent: 'center'
                 }}
               >
-                <TouchableOpacity
-                  onPress={() => this.takePicture(camera)}
-                  style={styles.capture}
-                >
-                  <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
+                {recording ? (
+                  <TouchableOpacity
+                    onPress={() => this.stopRecording(camera)}
+                    style={styles.capture}
+                  >
+                    <Text style={{ fontSize: 14 }}> STOP </Text>
+                  </TouchableOpacity>
+                ) : (
+                  <TouchableOpacity
+                    onPress={() => this.takeVideo(camera)}
+                    style={styles.capture}
+                  >
+                    <Text style={{ fontSize: 14 }}> SNAP </Text>
+                  </TouchableOpacity>
+                )}
               </View>
             );
           }}
@@ -51,13 +85,6 @@ class Camera extends Component {
       </View>
     );
   }
-
-  takePicture = async function(camera) {
-    const options = { quality: 0.5, base64: true };
-    const data = await camera.takePictureAsync(options);
-    //  eslint-disable-next-line
-    console.log(data.uri);
-  };
 }
 
 const styles = StyleSheet.create({
