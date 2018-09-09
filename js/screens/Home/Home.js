abe5cc5c0a40a7ef2b303d3035ddb0e1ba09eeb3
Dima Mukhin <dima9348@gmail.com>
2018-09-08 20:57:23 -0700

    added cyclist score

--- js/screens/Home/Home.js
+++ js/screens/Home/Home.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import { getCyclistById } from '../../services/redArtsClient';
 
 class Home extends Component {
+
+  state = {
+      cyclist: undefined
+  };
+
+  async componentDidMount() {
+    let cyclist =  await getCyclistById(3);
+    this.setState({ cyclist });
+  }
+
   render() {
     return (
       <View>
@@ -14,7 +25,7 @@ class Home extends Component {
           onAnimationComplete={() => console.log('onAnimationComplete')}
           backgroundColor="#3d5875"
         >
-          {fill => <Text>80</Text>}
+          {fill => <Text>{ this.state.cyclist ? this.state.cyclist.score : 0 }</Text>}
         </AnimatedCircularProgress>
         <Button
           title="Open Camera"
